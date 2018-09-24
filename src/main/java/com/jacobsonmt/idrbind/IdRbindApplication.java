a44c127e140389928e55f3e7019493603d2d4292
Matthew Jacobson <JacobsonMT@gmail.com>
2018-09-24 01:22:12 -0400

    Initial Commit

---
+++ src/main/java/com/jacobsonmt/idrbind/IdRbindApplication.java
@@ -0,0 +1,12 @@
+package com.jacobsonmt.idrbind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdRbindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdRbindApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jacobsonmt/idrbind/IdRbindApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jacobsonmt.idrbind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdRbindApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
