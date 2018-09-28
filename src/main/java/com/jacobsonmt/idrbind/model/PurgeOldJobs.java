2d20b12204e4ce7bf3db75ef72c34392a4d5abd4
Matthew Jacobson <JacobsonMT@gmail.com>
2018-09-27 23:05:32 -0400

    Job purge logging

--- src/main/java/com/jacobsonmt/idrbind/model/PurgeOldJobs.java
+++ src/main/java/com/jacobsonmt/idrbind/model/PurgeOldJobs.java
@@ -1,8 +1,11 @@
 package com.jacobsonmt.idrbind.model;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Iterator;
 import java.util.Map;
 
+@Log4j2
 public class PurgeOldJobs implements Runnable {
 
     private Map<String, IDRBindJob> savedJobs;
@@ -13,6 +16,7 @@ public class PurgeOldJobs implements Runnable {
 
     @Override
     public void run() {
+        int jobsPurged = 0;
         synchronized ( savedJobs ) {
             for ( Iterator<Map.Entry<String, IDRBindJob>> it = savedJobs.entrySet().iterator(); it.hasNext(); ) {
                 Map.Entry<String, IDRBindJob> entry = it.next();
@@ -21,9 +25,12 @@ public class PurgeOldJobs implements Runnable {
                     job.setSaved( false );
                     job.setSaveExpiredDate( null );
                     it.remove();
+                    log.debug( "Purged " + job.getJobId() );
+                    jobsPurged++;
                 }
             }
         }
+        log.info( "Purged " + Integer.toString( jobsPurged ) + " old jobs." );
     }
 
 }
